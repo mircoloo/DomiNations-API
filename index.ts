18437cc283154f9beca7431a23fef71c1d95b3ae
Filippo Benedetti <filippo.benedetti3@gmail.com>
2025-07-28 17:32:13 +0200

    Add ts support

--- index.ts
+++ index.ts
@@ -6,14 +6,26 @@ require('dotenv').config();
 
 // constants
 const mogooseUri = process.env.MONGOOSE_URI;
-const clientOptions = { serverApi: { version: '1', strict: true, deprecationErrors: true } };
+const clientOptions = {
+    serverApi: {
+        version: '1' as const,
+        strict: true,
+        deprecationErrors: true
+    }
+};
 const port = process.env.PORT || 3000;
 
 console.log("Starting Domination API...");
 async function run() {
     try {
+        if (!mogooseUri) {
+            throw new Error("MONGOOSE_URI non definito nelle variabili d'ambiente");
+        }
         // Create a Mongoose client with a MongoClientOptions object to set the Stable API version
         await mongoose.connect(mogooseUri, clientOptions);
+        if (!mongoose.connection.db) {
+            throw new Error("Connessione al database non riuscita");
+        }
         await mongoose.connection.db.admin().command({ ping: 1 });
         console.log("Mongoose connected successfully to mongoDB Atlas");
     } finally {
@@ -22,8 +34,8 @@ async function run() {
         console.log("Mongoose disconnected successfully from mongoDB Atlas");
     }
 }
+
 app.listen(port, () => {
     run().catch(console.dir);
     console.log(`Example app listening on port http://localhost:${port}`)
 })
-
